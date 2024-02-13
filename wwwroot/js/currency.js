c4bd0b35ce28f2e65ac4f5a6dfb764e03b895549
HLAUTOMATION <155465752+HLAUTOMATION@users.noreply.github.com>
2024-02-13 16:36:48 +0200

    add wwwroot/

---
+++ wwwroot/js/currency.js
@@ -0,0 +1,39 @@
+var currencyInput = document.querySelector('input[type="currency"]')
+
+var currency = 'CLP' // https://www.currency-iso.org/dam/downloads/lists/list_one.xml
+
+// format inital value
+onBlur({ target: currencyInput } )
+
+// bind event listeners
+currencyInput.addEventListener('focus', onFocus)
+currencyInput.addEventListener('blur', onBlur)
+
+
+
+function localStringToNumber(s) {
+    return Number(String(s).replace(/[^0-9.-]+/g, ""))
+}
+
+function onFocus(e) {
+    var value = e.target.value;
+    e.target.value = value ? localStringToNumber(value) : ''
+}
+
+function onBlur(e) {
+    var value = e.target.value
+
+    var options = {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+        currency: currency,
+        style: "currency",
+        currencyDisplay: "symbol"
+    }
+
+    e.target.value = (value || value === 0)
+        ? localStringToNumber(value).toLocaleString(undefined, options)
+        : ''
+} ElementById("numUSD").textContent = result;
+
+
---
+++ wwwroot/js/effect.js
@@ -0,0 +1,13 @@
+﻿const onhold = document.querySelector('#onhold');
+const comentario = document.querySelector('#comentario');
+
+onhold.addEventListener(
+    'click',
+    () => {
+        if (comentario.style.display === 'none') {
+            comentario.style.display = 'block';
+        } else {
+            comentario.style.display = 'none';
+        }
+    }
+);
